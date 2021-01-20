0f1b39d40de6aa2342ebabcf24ee9e5d36009fbd
ycjcl868 <45808948@qq.com>
2021-01-20 18:12:23 +0800

    chore: test

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const {owner, repo} = github.context.repo
+    core.info(`owner: ${owner}, repo: ${repo}`)
+    const token: string = core.getInput('token')
+    core.debug(`Waiting ${token} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let myOutput = ''
+    let myError = ''
+
+    const options: exec.ExecOptions = {
+      cwd: process.cwd(),
+      listeners: {
+        stdout: (data: Buffer) => {
+          myOutput += data.toString()
+        },
+        stderr: (data: Buffer) => {
+          myError += data.toString()
+        }
+      }
+    }
+    if (myError) {
+      core.error(myError)
+    }
+    await exec.exec('yarn', ['outdated'], options)
+    core.info(myOutput)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
